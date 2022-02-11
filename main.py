d8795f9f9dd490349a74436cabc60d89f80aa24d
stefanvanberkum <40003032+stefanvanberkum@users.noreply.github.com>
2022-02-11 11:44:37 +0100

    Updated code.

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def main():
     var_start = 1
     cov_start = 0
     cov_rests = ['RC', 'IDO', 'IDE']
-    cov_types = ['opg', 'oim', 'approx', 'robust', 'approx_robust']
+    cov_type = 'oim'
 
     # model = SSMS(test_data, group_name='Region', y_name='SalesGoodsEUR', z_names=z_names, c_names=c_names,
     #             cov_rest='IDE', var_start=1, cov_start=0, fancy_start=True)
@@ -66,64 +66,65 @@ def main():
     for data_type in data_options:
         data = data_options[data_type]
         for cov_rest in cov_rests:
-            for cov_type in cov_types:
-                print("Running " + cov_rest + "_" + cov_type + " with " + data_type + " data...")
+            print("Running " + cov_rest + " with " + data_type + " data...")
+            split_time = time.time()
 
-                model = SSMS(data, group_name='Region', y_name='SalesGoodsEUR', z_names=z_names, c_names=c_names,
-                             cov_rest=cov_rest, var_start=var_start, cov_start=cov_start, fancy_start=True)
-                result = model.fit(maxiter=1000, maxfun=1000000, cov_type=cov_type, disp=-1)
+            model = SSMS(data, group_name='Region', y_name='SalesGoodsEUR', z_names=z_names, c_names=c_names,
+                         cov_rest=cov_rest, var_start=var_start, cov_start=cov_start, fancy_start=True)
+            result = model.fit(maxiter=10000, maxfun=10000000, cov_type=cov_type, disp=-1)
 
-                # Make sure to plot the same regions each time.
-                grouped = data.groupby('Region', sort=False)
-                group_names = [name for name, group in grouped]
-                region_1 = group_names.index('NL310_503')
-                region_2 = group_names.index('NL33C_340')
-                region_3 = group_names.index('NL33C_506')
-                region_4 = group_names.index('NL212_507')
+            # Make sure to plot the same regions each time.
+            grouped = data.groupby('Region', sort=False)
+            group_names = [name for name, group in grouped]
+            region_1 = group_names.index('NL310_503')
+            region_2 = group_names.index('NL33C_340')
+            region_3 = group_names.index('NL33C_506')
+            region_4 = group_names.index('NL212_507')
 
-                y_pred = result.get_prediction(start=10, end=190)
-                y1_pred = y_pred.predicted_mean[:, region_1]
-                y2_pred = y_pred.predicted_mean[:, region_2]
-                y3_pred = y_pred.predicted_mean[:, region_3]
-                y4_pred = y_pred.predicted_mean[:, region_4]
-                mse_1 = np.mean(np.square(model.endog[10:, region_1] - y1_pred))
-                mse_2 = np.mean(np.square(model.endog[10:, region_2] - y2_pred))
-                mse_3 = np.mean(np.square(model.endog[10:, region_3] - y3_pred))
-                mse_4 = np.mean(np.square(model.endog[10:, region_4] - y4_pred))
+            y_pred = result.get_prediction(start=10, end=190)
+            y1_pred = y_pred.predicted_mean[:, region_1]
+            y2_pred = y_pred.predicted_mean[:, region_2]
+            y3_pred = y_pred.predicted_mean[:, region_3]
+            y4_pred = y_pred.predicted_mean[:, region_4]
+            mse_1 = np.mean(np.square(model.endog[10:, region_1] - y1_pred))
+            mse_2 = np.mean(np.square(model.endog[10:, region_2] - y2_pred))
+            mse_3 = np.mean(np.square(model.endog[10:, region_3] - y3_pred))
+            mse_4 = np.mean(np.square(model.endog[10:, region_4] - y4_pred))
 
-                mses = np.zeros(len(group_names))
-                for region in range(len(group_names)):
-                    mses[region] = np.mean(np.square(model.endog[10:, region] - y_pred.predicted_mean[:, region]))
-                mse = np.mean(mses)
+            mses = np.zeros(len(group_names))
+            for region in range(len(group_names)):
+                mses[region] = np.mean(np.square(model.endog[10:, region] - y_pred.predicted_mean[:, region]))
+            mse = np.mean(mses)
 
-                t = np.arange(11, 192)
-                fig, axes = plt.subplots(2, 2)
-                axes[0, 0].set_title('mse: {0}'.format(format(mse_1, '.4f')))
-                axes[0, 0].set_xticks([])
-                axes[0, 0].plot(t, model.endog[10:, region_1], 'b')
-                axes[0, 0].plot(t, y1_pred, 'r')
-                axes[0, 1].set_title('mse: {0}'.format(format(mse_2, '.4f')))
-                axes[0, 1].set_xticks([])
-                axes[0, 1].plot(t, model.endog[10:, region_2], 'b')
-                axes[0, 1].plot(t, y2_pred, 'r')
-                axes[1, 0].set_title('mse: {0}'.format(format(mse_3, '.4f')))
-                axes[1, 0].plot(t, model.endog[10:, region_3], 'b')
-                axes[1, 0].plot(t, y3_pred, 'r')
-                axes[1, 1].set_title('mse: {0}'.format(format(mse_4, '.4f')))
-                axes[1, 1].plot(t, model.endog[10:, region_4], 'b')
-                axes[1, 1].plot(t, y4_pred, 'r')
-                name = '_'.join([cov_rest, cov_type, data_type])
-                fig.suptitle('{0} (mse: {1})'.format(name, format(mse, '.4f')))
-                plt.savefig(os.path.join(save_path, name), dpi=300, format='png')
-                np.savetxt(os.path.join(save_path, name + '.csv'), result.params, delimiter=',')
-                plt.close('all')
+            t = np.arange(11, 192)
+            fig, axes = plt.subplots(2, 2)
+            axes[0, 0].set_title('mse: {0}'.format(format(mse_1, '.4f')))
+            axes[0, 0].set_xticks([])
+            axes[0, 0].plot(t, model.endog[10:, region_1], 'b')
+            axes[0, 0].plot(t, y1_pred, 'r')
+            axes[0, 1].set_title('mse: {0}'.format(format(mse_2, '.4f')))
+            axes[0, 1].set_xticks([])
+            axes[0, 1].plot(t, model.endog[10:, region_2], 'b')
+            axes[0, 1].plot(t, y2_pred, 'r')
+            axes[1, 0].set_title('mse: {0}'.format(format(mse_3, '.4f')))
+            axes[1, 0].plot(t, model.endog[10:, region_3], 'b')
+            axes[1, 0].plot(t, y3_pred, 'r')
+            axes[1, 1].set_title('mse: {0}'.format(format(mse_4, '.4f')))
+            axes[1, 1].plot(t, model.endog[10:, region_4], 'b')
+            axes[1, 1].plot(t, y4_pred, 'r')
+            name = '_'.join([cov_rest, data_type])
+            fig.suptitle('{0} (mse: {1})'.format(name, format(mse, '.4f')))
+            plt.savefig(os.path.join(save_path, name), dpi=300, format='png')
+            plt.close('all')
 
-                print_results(result, save_path, 'test')
-                result.save(os.path.join(save_path, name + '.pickle'))
+            print_results(result, save_path, name)
+            result.save(os.path.join(save_path, name + '.pickle'))
 
-                print("Done!")
-                end_time = time.time()
-                print("Runtime:", (end_time - start_time), sep=' ')
+            print("Done!")
+            end_time = time.time()
+            print("Runtime:", (end_time - split_time), sep=' ')
+    end_time = time.time()
+    print("Total runtime:", (end_time - start_time), sep=' ')
 
 
 def run_exploratory(data, save_path):
