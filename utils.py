9cda2cc73ec5cd6cd98cded165148b655602315b
TibeIritie <95891191+TibeIritie@users.noreply.github.com>
2022-02-18 00:52:20 +0100

    Added plot_states, mse_forecast and use_pickle statement
    plot_states can also be used for in-sample predictions from t=1 till t=190 (that's why I removed all plotting lines in main) and plot_states should work with small modifications for out-of-sample predictions in the future.

--- utils.py
+++ utils.py
@@ -2,13 +2,118 @@
 This module provides utility methods.
 """
 import os
+import datetime
 
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from statsmodels.tsa.statespace.mlemodel import MLEResults
+from plotnine import *
 
 from state_space import SSMS, SSMS_alt, SSMS_alt_4
+
+
+def plot_states(results: MLEResults, regions: list, z_names: list, save_path: str):
+    """
+    Plots states (all variables specified in z_names) and saves it in save_path.
+    The dataframe states contains all the states (mu, nu, z_names) over time.
+
+    :param results: train results from a SSMS_alt_4 class
+    (this function might not work for with results from other classes)
+    :param regions: list of region names
+    :param z_names: a list of column names of the independent variables to be placed in the Z (design) matrix
+    :param save_path: save path for plots
+    :return:
+    """
+    n_regions = len(regions)
+    n_betas = len(z_names)
+    cols = []
+    for i in range(results.states.filtered.shape[1]):
+        if i < n_regions:
+            cols.append('nu_'+regions[i])
+        elif n_regions <= i < n_regions*2:
+            cols.append('mu_'+regions[i-n_regions])
+        else:
+            cols.append(z_names[i-n_regions*2])
+    states = pd.DataFrame(results.states.filtered, columns=cols)
+    states['Date'] = pd.date_range(start='1/1/2018', periods=len(states), freq='W')
+    # The first 13 observations are removed for nice graphs
+    states = states.iloc[13:, :]
+    for i in range(n_betas):
+        p = ggplot(states, aes(x='Date', y=states.columns[n_regions*2+i])) \
+            + scale_x_date(date_labels="%Y-%W") \
+            + geom_line() \
+            + labs(x='Date', y=states.columns[n_regions*2+i])
+        # print(p)
+        ggsave(plot=p, filename=states.columns[n_regions*2+i], path=save_path, verbose=False)
+
+
+def mse_forecast(results: MLEResults, model, regions: list, save_path: str, first=int, last=int, ci=bool, tp=str):
+    """
+    Computes MSE with one-step ahead forecasts for each region and saves it in save_path.
+    Plots SalesGoodsEUR with tp for regions indices specified in plot_regions and saves it in save_path.
+    For now only the plots for the regions with the largest/smallest MSE are saved.
+
+    :param results: (extended) results (from prepare_forecast())
+    :param model: (extended) model (from prepare_forecast())
+    :param regions: list of region names,
+    the order of the names should be exactly the same as the order of the regions in the model
+    :param save_path: save path for plots
+    :param first: the time index from where your plots should start
+    :param last: this time index should exactly be equal to the time index-1 where the sample of the model ends
+    :param ci: whether to plot a confidence interval (True) or not (False),
+    if the CI's become too big set ci=False otherwise the sales will be plotted as straight lines
+    :param tp: specify the type of data (e.g. in_sample_prediction or one_step_ahead_forecast) you want to plot,
+    use _ instead of spaces in for tp, since the name of the plots/excel files will also have this name
+    :return:
+    """
+
+    n_regions = len(regions)
+    data = results.get_prediction(start=first, end=last)
+
+    # Calculate MSE use one-step ahead forecasts
+    mses = np.zeros(len(regions))
+    for region in range(len(regions)):
+        mses[region] = np.mean(np.square(model.endog[first:, region] - data.predicted_mean[:, region]))
+    mse = np.mean(mses)
+    best, worst = np.argmin(mses), np.argmax(mses)
+    pd.DataFrame(mses.reshape(1, n_regions), columns=regions).to_excel(os.path.join(save_path, 'mses_' + tp + '.xlsx'))
+    print(f'{tp}s starting from t={first}')
+    print(f'Average MSE of regions : {mse}')
+    print(f'Region with smallest MSE: {regions[best]}, {mses[best]}')
+    print(f'Region with largest MSE: {regions[worst]}, {mses[worst]}')
+    print()
+
+    # Plot data for regions
+    df = pd.DataFrame(np.concatenate((model.endog[first:, :], data.predicted_mean, data.conf_int()), axis=1))
+    start_date = datetime.datetime(2018, 1, 1) + datetime.timedelta(weeks=first)
+    df['Date'] = pd.date_range(start=start_date, periods=len(df), freq='W')
+    # Remove the first two rows (first two observations starting from first)
+    # such that the dates on the x-axis are nicely plotted
+    if first == 153:
+        df = df[3:]
+    # Specify the indices of the regions you want to plot in plot_regions
+    plot_regions = [best, worst]
+    for i in range(len(plot_regions)):
+        if ci:
+            p = ggplot(df, aes(x='Date')) \
+                + scale_x_date(date_labels="%Y-%W") \
+                + geom_ribbon(
+                aes(ymin=df.iloc[:, n_regions * 2 + plot_regions[i]], ymax=df.iloc[:, n_regions * 3 + plot_regions[i]],
+                    color='"95% CI"'), alpha=0.1) \
+                + geom_line(aes(y=df.iloc[:, plot_regions[i]], color='"Actual"')) \
+                + geom_line(aes(y=df.iloc[:, n_regions + plot_regions[i]], color='"Forecast"')) \
+                + scale_color_manual(values=['#dedede', '#4472c4', '#ed7d31']) \
+                + labs(x='Date', y='Sales', color='Legend')
+        else:
+            p = ggplot(df, aes(x='Date')) \
+                + scale_x_date(date_labels="%Y-%W") \
+                + geom_line(aes(y=df.iloc[:, plot_regions[i]], color='"Actual"')) \
+                + geom_line(aes(y=df.iloc[:, n_regions + plot_regions[i]], color='"Forecast"')) \
+                + scale_color_manual(values=['#4472c4', '#ed7d31']) \
+                + labs(x='Date', y='Sales', color='Legend')
+        # print(p)
+        ggsave(plot=p, filename=regions[plot_regions[i]] + '_' + tp, path=save_path, verbose=False)
 
 
 def print_results(results: MLEResults, save_path: str, name: str):
@@ -230,4 +335,4 @@ def prepare_forecast(results: MLEResults, data: pd.DataFrame):
                            cov_rest=model.cov_rest)
     fitted_params = results.params
     new_result = new_model.filter(fitted_params)
-    return new_result
+    return new_model, new_result
